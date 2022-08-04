3baed7b40c3cfd59e08ec9cad67fa8caf37ad68b
Tatu-Armadillo <prohazor.fa@gmail.com>
2022-08-04 08:42:26 -0300

    migrations files. V1, V2, V3, V4

---
+++ UdemyCurso/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES 
+(1,'Algum lugar ai','Dedo','Dedinho','Male'),
+(2,'Algum lugar ai','Cacatua','passaro','Female'),
+(5,'Algum lugar ai','Fone Razor','Item','Male'),
+(6,'Algum lugar ai','AAAAAAAAA','BBBBBBBB','Mele');
